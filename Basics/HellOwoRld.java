7f21a85ddf07debf219724a6581a52c8520c6e52
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-03 10:46:39 +0530

    Create HellOwoRld.java
    Code has Emotions attached to it beware....

---
+++ Basics/HellOwoRld.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+//Class name in Camel case
+// public keyword makes it accessible anywhere within program but anyone
+public class Main
+{
+//   static is its not neccessary to create a seperate object to run it
+//   void is it doesnt return anything
+//   main is our function name proper naming uses pascalCase 
+//   args are for commandline args but this is just beginning and i am just revising.
+//   S.out is actually the standard output method i dont really know how it works uptil now
+//   that's it for HellOwoRld
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
