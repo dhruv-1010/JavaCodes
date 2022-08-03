31e4a6b26eee676c0c620a2acf3dbc86bbd982a0
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-03 20:00:18 +0530

    Create Q5.java

---
+++ Basics/practiceset1/Q5.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Number ");
+	    boolean a = sc.hasNextInt();
+// 	    System.out.println("Enter Marks in Maths");
+// 	    int b = sc.nextInt();
+// 	    System.out.println("Enter Marks in Hindi");
+// 	    int c = sc.nextInt();   
+		System.out.print(a);
+
+	}
+}
