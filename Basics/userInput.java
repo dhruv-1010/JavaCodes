003825ac2262bb0429ac99d55967dadeef89b0df
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-03 12:12:33 +0530

    Scanner is needed

---
+++ Basics/userInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+// I don't know why but am i learning this. just for marks '-' 
+// most imp ***
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in); //creating scanner object 
+	   // System.out.println("Enter  num");
+	   // int a = sc.nextInt();
+	   // int b = sc.nextInt();
+	   // int sum = a;
+	   // System.out.println("their sum");
+	   // boolean check = sc.hasNextInt();
+	   // System.out.println(check);
+	    String abc = sc.nextLine();
+	    System.out.println(abc);
+	   // System.out.println(sum);
+	    
+	}
+}
