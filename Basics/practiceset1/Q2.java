ae8c44004349cce4decdd0b64bb82fa6459c1aba
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-03 19:54:39 +0530

    Create Q2.java

---
+++ Basics/practiceset1/Q2.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Marks in English");
+	    int a = sc.nextInt();
+	    System.out.println("Enter Marks in Maths");
+	    int b = sc.nextInt();
+	    System.out.println("Enter Marks in Hindi");
+	    int c = sc.nextInt();   
+		System.out.print("Your GPA is = ");
+		double sum = a+b+c;
+	    System.out.print((sum/3)/10);
+	}
+}
