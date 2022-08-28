b279f47cf9016741f00c81115097fa5261038fd6
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-28 16:42:40 +0530

    Create ScannerClass.java

---
+++ Basics/ScannerClass.java
@@ -0,0 +1,29 @@
+// read input from stdin is by using the Scanner class and specifying the Input Stream as System.in.
+// Scanner scanner = new Scanner(System.in);
+// String myString = scanner.next();
+// int myInt = scanner.nextInt();
+// scanner.close();
+
+// System.out.println("myString is: " + myString);
+// System.out.println("myInt is: " + myInt);
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        
+        System.out.println(b);
+        
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
